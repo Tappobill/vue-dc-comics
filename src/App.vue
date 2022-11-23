adaab07f0230111a75ffc7e8273dbae96b53d665
Tappobill <christian.salvati@hotmail.it>
2022-11-23 18:44:27 +0100

    Header

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HeadComp />
+    <MainComp />
+    <FootComp />
+  </div>
+</template>
+
+<script>
+import HeadComp from './components/HeadComp.vue'
+import MainComp from './components/MainComp.vue'
+import FootComp from './components/FootComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeadComp,
+    MainComp,
+    FootComp
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeadComp.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="header">
+
+        <div class="logo-head">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+
+        <div class="lista-head">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeadComp'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variabili.scss";
+@import "../assets/style/mixin.scss";
+
+* {
+   @include reset($reset:'standard');
+}
+
+.header {
+    @include box80($box80: 'standard');
+    display: flex;
+    justify-content: space-around;
+}
+
+ul {
+    display: flex;
+    margin-top: 35px;
+
+    li {
+        margin: $margin;
+        padding: $pad;
+        font-size: 10px;
+
+        a {
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+        }
+    }    
+}
+
+a:hover{
+    color: cornflowerblue;
+    cursor: pointer;
+    padding-bottom: 40px;
+    border-bottom: 5px solid cornflowerblue;
+}
+</style>
